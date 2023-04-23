6856e70d2a698a01da594c2ae9399886a9300510
itscopebd <itscopebd@gmail.com>
2023-04-23 16:38:59 +0600

    haeader component added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../Components/Header';
+import HomePage from './../Pages/HomePage/HomePage';
+
+const Main = () => {
+    return (
+        <div className='container mx-auto'>
+
+            <Header></Header>
+            <HomePage></HomePage>
+
+
+        </div>
+    );
+};
+
+export default Main;
