046b495eb110a25c1520a27cd02697f4ce8f8176
itscopebd <itscopebd@gmail.com>
2023-07-15 00:34:27 +0600

    Banner section slider and grid layout added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,15 +1,77 @@
 import React from 'react';
-
+import featuredImage from "../../assets/images/featured.png";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
 const Banner = () => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        loop: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+    };
+
     return (
         <div>
-            <div className="grid grid-cols-2 gap-4">
-            
-                <div className="col-span-8 row-span-4">
-                    <img src="image/ARTHONITI_2.png" alt="" />
+            <div class="grid grid-cols-4 gap-4">
+
+                <div class="col-span-2 row-span-6 bg-red-500 rounded-md relative">
+                    <Slider {...settings}>
+                        <div>
+                            <img className='w-full h-full rounded-md' src={featuredImage} alt="" />
+                            <div className='bg-gray-400 text-center flex items-center justify-center bg-opacity-50 h-24 absolute bottom-0 w-full'>
+                                <h4 className='text-4xl text-black'>Title Here </h4>
+                            </div>
+                        </div>
+                        <div>
+                            <img className='w-full h-full rounded-md' src={featuredImage} alt="" />
+                            <div className='bg-gray-400 text-center flex items-center justify-center bg-opacity-50 h-24 absolute bottom-0 w-full'>
+                                <h4 className='text-4xl text-black'>Title Here </h4>
+                            </div>
+                        </div>
+                        <div>
+                            <img className='w-full h-full rounded-md' src={featuredImage} alt="" />
+                            <div className='bg-gray-400 text-center flex items-center justify-center bg-opacity-50 h-24 absolute bottom-0 w-full'>
+                                <h4 className='text-4xl text-black'>Title Here </h4>
+                            </div>
+                        </div>
+                        <div>
+                            <img className='w-full h-full rounded-md' src={featuredImage} alt="" />
+                            <div className='bg-gray-400 text-center flex items-center justify-center bg-opacity-50 h-24 absolute bottom-0 w-full'>
+                                <h4 className='text-4xl text-black'>Title Here </h4>
+                            </div>
+                        </div>
+                    </Slider>
                 </div>
-        
-                <div className="">07</div>
+                <div class="col-span-1 row-span-3 bg-red-500 rounded-md relative">
+                    <img className='w-full rounded-md' src={featuredImage} alt="" />
+                    <div className='bg-gray-400 text-center flex items-center justify-center bg-opacity-50 h-24 absolute bottom-0 w-full'>
+                        <h4 className='text-4xl text-black'>Title Here </h4>
+                    </div>
+                </div>
+                <div class="col-span-1 row-span-3 bg-red-500 rounded-md relative">
+                    <img className='w-full rounded-md' src={featuredImage} alt="" />
+                    <div className='bg-gray-400 text-center flex items-center justify-center bg-opacity-50 h-24 absolute bottom-0 w-full'>
+                        <h4 className='text-4xl text-black'>Title Here </h4>
+                    </div>
+                </div>
+                <div class="col-span-1 row-span-3 bg-red-500 rounded-md relative">
+                    <img className='w-full rounded-md' src={featuredImage} alt="" />
+                    <div className='bg-gray-400 text-center flex items-center justify-center bg-opacity-50 h-24 absolute bottom-0 w-full'>
+                        <h4 className='text-4xl text-black'>Title Here </h4>
+                    </div>
+                </div>
+                <div class="col-span-1 row-span-3 bg-red-500 rounded-md relative">
+                    <img className='w-full rounded-md' src={featuredImage} alt="" />
+                    <div className='bg-gray-400 text-center flex items-center justify-center bg-opacity-50 h-24 absolute bottom-0 w-full'>
+                        <h4 className='text-4xl text-black'>Title Here </h4>
+                    </div>
+                </div>
             </div>
         </div>
     );
